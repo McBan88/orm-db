6eda419dfaf0ba29fac44b7d66d1eda07207d174
McBan88 <maciusb@gmail.com>
2023-04-17 15:56:28 +0200

    DB config

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,22 @@ import { EmployeesModule } from './employees/employees.module';
 import { CompaniesModule } from './companies/companies.module';
 import { MeetingsModule } from './meetings/meetings.module';
 import { EmployeesMeetingsModule } from './employees_meetings/employees_meetings.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CarsModule, EmployeesModule, CompaniesModule, MeetingsModule, EmployeesMeetingsModule],
+  imports: [
+    CarsModule,
+    EmployeesModule,
+    CompaniesModule,
+    MeetingsModule,
+    EmployeesMeetingsModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: ['dist/**/*.entity.js'],
+      sychronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
